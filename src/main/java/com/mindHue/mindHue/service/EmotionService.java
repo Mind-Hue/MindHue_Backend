0d2e6e9ef126d703bd2e3d2acbd0d50fdbf84f7a
Paola Morales <dissenyaspasia@gmail.com>
2025-05-16 10:47:13 +0200

    Add complete CRUD functionality for Emotion entity

--- src/main/java/com/mindHue/mindHue/service/EmotionService.java
+++ src/main/java/com/mindHue/mindHue/service/EmotionService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import com.mindhue.mindhue.model.Emotion;
 import com.mindhue.mindhue.repository.EmotionRepository;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 
 
@@ -26,5 +28,31 @@ public class EmotionService {
 
     public ResponseEntity<Object> getAllEmotions() {
         return new ResponseEntity<>(emotionRepository.findAll(), HttpStatus.OK);
+    } 
+
+    public Optional<Emotion> getEmotionById(int id) {
+        return emotionRepository.findById(id);
     }
+
+    public ResponseEntity<Object> updateEmotion(int id, Emotion emotion) {
+        Optional<Emotion> existingEmotion = emotionRepository.findById(id);
+        if (existingEmotion.isPresent()) {
+            emotion.setId(id);
+            emotionRepository.save(emotion);
+            return new ResponseEntity<>(emotion, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Emotion not found", HttpStatus.NOT_FOUND);
+        }
+    }
+    public ResponseEntity<Object> deleteEmotion(int id) {
+        Optional<Emotion> existingEmotion = emotionRepository.findById(id);
+        if (existingEmotion.isPresent()) {
+            emotionRepository.deleteById(id);
+            return new ResponseEntity<>("Emotion deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Emotion not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+   
 }
